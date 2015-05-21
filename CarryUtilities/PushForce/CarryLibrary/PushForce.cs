297286d004d6b070d468e9b2de9abb6003623a24
Romesti <kelex.pub@gmail.com>
2015-05-21 06:34:06 +0200

    First commit carryutiities

---
+++ CarryUtilities/PushForce/CarryLibrary/PushForce.cs
@@ -0,0 +1,114 @@
+﻿using CarryLibrary.Enumerations;
+using LeagueSharp;
+using LeagueSharp.SDK.Core;
+using LeagueSharp.SDK.Core.Managers;
+using LeagueSharp.SDK.Core.UI.Notifications;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarryLibrary
+{
+    public static class PushForce
+    {
+        private static readonly List<PushedLaneArgs> LanePushForce = new List<PushedLaneArgs>{
+                new PushedLaneArgs{Lane=Lane.Top,PushingForce=0,Frontline=new SharpDX.Vector3(2192f,12608f,52.83f)},
+                new PushedLaneArgs{Lane=Lane.Mid,PushingForce=0,Frontline=new SharpDX.Vector3(7524f,7426f,53.43f)},
+                new PushedLaneArgs{Lane=Lane.Bottom,PushingForce=0,Frontline=new SharpDX.Vector3(12566f,2390f,51.68f)}
+        };
+        public  delegate void OnPushedLaneDelegate(PushedLaneArgs e);
+        public static event OnPushedLaneDelegate OnPushedLane;
+
+        private  const double MELEE_VALUE = 2;
+        private  const double RANGED_VALUE = 4;
+        private  const double CHAMP_VALUE = 20;
+        private static Notification _not = new Notification("Notif",100,false);
+
+        private static bool IsSubscribed { get { return OnPushedLane != null && OnPushedLane.GetInvocationList().Count() > 0; } }
+
+        static PushForce()
+        {
+            Game.OnUpdate += PushForce.OnUpdate;
+            Notifications.AddNotification(_not);
+
+        }
+
+        static void OnUpdate(EventArgs args)
+        {
+            if (IsSubscribed)
+            {
+                var minions = ObjectHandler.GetFast<Obj_AI_Minion>()
+                .Where(minion => minion.Team != GameObjectTeam.Neutral
+                        && (!minion.IsStunned && !minion.IsRooted && !minion.IsMoving)
+                        && minion.IsMinion)
+                        //.GroupBy(minion => minion.Name)
+                        .Sum(minion => (minion.IsMeele ?MELEE_VALUE:RANGED_VALUE)*minion.HealthPercent);
+               /* foreach (var laneArgs in LanePushForce.ToList())
+                {
+                    double s = 0;
+
+                    if (minions.FirstOrDefault(m => m.IsAlly) == null || minions.FirstOrDefault(m => m.IsEnemy) == null) continue;
+                    Console.WriteLine("lel");
+
+                    Notifications.UpdateNotifications();
+                    foreach (var minion in minions)
+                    {
+                        if (minion.IsAlly)
+                        {
+                            s += MELEE_VALUE * minion.HealthPercent;
+                        }
+                        else if (minion.IsEnemy)
+                        {
+                            s -= MELEE_VALUE * minion.HealthPercent;
+                        }
+                    }
+
+                    var frontLine = (minions.First(m=>m.IsAlly).Position - minions.First(m=>m.IsEnemy).Position);
+                    frontLine.Normalize();
+
+                    minions = MinionManager.GetMinions(
+                        laneArgs.Frontline,
+                        2000,
+                        LeagueSharp.SDK.Core.Enumerations.MinionTypes.Ranged,
+                        LeagueSharp.SDK.Core.Enumerations.MinionTeam.All,
+                        LeagueSharp.SDK.Core.Enumerations.MinionOrderTypes.None);
+
+
+                    foreach (var minion in minions)
+                    {
+                        if (minion.IsAlly)
+                        {
+                            s += RANGED_VALUE * minion.HealthPercent;
+                        }
+                        else if (minion.IsEnemy)
+                        {
+                            s -= RANGED_VALUE * minion.HealthPercent;
+                        }
+                    }
+
+                    var a = LanePushForce.FindIndex(laneA => laneA.Lane == laneArgs.Lane);
+                    LanePushForce[a] = new PushedLaneArgs { Lane = laneArgs.Lane,PushingForce=s,Frontline=frontLine };
+
+                    if (LanePushForce[a].PushingForce > 0)
+                    {
+                        OnPushedLane(laneArgs);
+                    }
+                }
+                */
+            }
+        }
+
+       
+
+        public class PushedLaneArgs : EventArgs
+        {
+            public Lane Lane { get; set; }
+
+            public double PushingForce { get; set; }
+
+            public SharpDX.Vector3 Frontline {get;set;}
+        }
+    }
+}
---
+++ CarryUtilities/PushForce/PushForce/Program.cs
@@ -0,0 +1,72 @@
+﻿using LeagueSharp.Common;
+using LeagueSharp;
+using SharpDX;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PushForce
+{
+    class Program
+    {
+
+        private static Menu menu;
+        static void Main(string[] args)
+        {
+            LeagueSharp.Common.CustomEvents.Game.OnGameLoad += GameLoad;
+            
+        }
+
+        private static void GameLoad(EventArgs args)
+        {
+ 	        CarryLibrary.PushForce.OnPushedLane += OnPushed;
+            LeagueSharp.Game.OnUpdate += onGameUpdate;
+            Drawing.OnDraw += Drawing_OnDraw;
+
+            menu = new Menu("PushForce", "pushforce", true);
+            menu.AddItem(new MenuItem("getCurrentPosition", "getCurrentPosition", false).SetValue(new KeyBind(84, KeyBindType.Press)));
+            menu.AddToMainMenu();
+        }
+
+        private static void Drawing_OnDraw(EventArgs args)
+        {
+            foreach (var lane in FrontLines.Keys)
+            {
+                Drawing.DrawCircle(FrontLines[lane], 100, System.Drawing.Color.White);
+            }
+        }
+
+        private static void onGameUpdate(EventArgs args)
+        {
+            if (menu.Item("getCurrentPosition").GetValue<KeyBind>().Active)
+            {
+                var minions = MinionManager.GetMinions(1000, team: MinionTeam.All);
+                string s = "";
+                if (minions.FirstOrDefault(m => m.IsEnemy) != null)
+                {
+                    s += "enemy " + minions.FirstOrDefault(m => m.IsEnemy);
+                }
+                if (minions.FirstOrDefault(m => m.IsAlly) != null)
+                {
+                    s += " ally " + minions.FirstOrDefault(m => m.IsAlly);
+                }
+                if(s != "")
+                    Notifications.AddNotification(s,1000);
+            }
+
+        }
+
+        private static IDictionary<CarryLibrary.Enumerations.Lane, Vector3> FrontLines = new Dictionary<CarryLibrary.Enumerations.Lane,Vector3>();
+
+        private static void OnPushed(CarryLibrary.PushForce.PushedLaneArgs e)
+        {
+            Notifications.AddNotification(String.Format("Lane {0} is pushed ! factor {1}",e.Lane.ToString(),e.PushingForce),1000);
+            FrontLines[e.Lane] = e.Frontline;
+
+        }
+
+
+    }
+}
